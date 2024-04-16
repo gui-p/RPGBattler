9e7f1c3bf527892b4103894446c38a5022b5bac3
KaioOliveiraDomingues <kaiooliveiradomingues1@gmail.com>
2024-04-15 23:10:28 -0300

    added "inventory-feature" branch

---
+++ RolePlayBattler.Domain/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RolePlayBattler.Domain
+{
+
+    //Inventario geral (to pensando ainda)
+    public class Inventory
+    {
+        public int SlotsCount { get; set; }
+        public String[]? Slots { get; private set; }
+        
+    }
+
+    //Ideia atual é que os itens sejam tratados como objetos da classe itens, lista de Enum pra tipo de item (ajuda a definir quais itens podem ir em quais slots, consumiveis, etc)
+    public class EquipmentSlots
+    {
+
+    }
+}
---
+++ RolePlayBattler.Domain/Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RolePlayBattler.Domain
+{ 
+
+    //definir caracteristicas gerais pra todos os itens, precisam ser caracteristicas que de pra encaixar qualquer coisa dentro
+    public class Items
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public int ItemCount { get; set; }
+        public enum ItemType
+        {
+            Weapon, Helmet, ChestPlate, Gloves, Pants, Boots, Consumable, Misc
+        }
+
+        // a ideia do item effect é por exemplo: 8 em espada é 8 de dano, mas se for uma armadura se torna 8 de defesa,
+        // em consumivel se torna 8 de cura, etc. (talvez seja necessario especificar mais depois para itens muito
+        // especificos, se for o caso só utilizar herança dessa classe)
+        public int ItemEffect {  get; set; }
+    }
+
+}
